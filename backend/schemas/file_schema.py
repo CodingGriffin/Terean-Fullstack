5cb3ae05066692e4f863c4748d5f67878b85aff6
dkb737@gmail.com <dkb737@gmail.com>
2025-05-26 18:24:27 -0700

    Create a DB model for generic files.

---
+++ backend/schemas/file_schema.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+
+class FileBase(BaseModel):
+    id: str
+    original_name: str
+    path: str
+    size: int
+    upload_date: datetime = None
+    mime_type: str
+    file_extension: str
+    project_id: Optional[str] = None
+
+
+class FileCreate(FileBase):
+    pass
+
+
+class File(FileBase):
+    class Config:
+        from_attributes = True 
