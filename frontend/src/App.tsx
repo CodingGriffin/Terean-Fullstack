1f3633540a2a07f8b120f585f6fa6cc6acd44c63
dkb737@gmail.com <dkb737@gmail.com>
2025-06-05 02:46:08 -0700

    Use new context in base Project Page, and remove the temporary V2 page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -23,7 +23,6 @@ import ProjectPage from "./Pages/Projects/ProjectPage.tsx";
 import ProjectsPage from "./Pages/Projects/ProjectsPage.tsx";
 import DataManagementPage from "./Pages/Projects/DataManagementPage.tsx";
 import SendResultsEmailPage from "./Pages/Projects/SendResultsEmailPage.tsx";
-import ProjectPage_v2 from "./Pages/Projects_v2/ProjectPage_v2.tsx";
 
 export default function App() {
   return (
@@ -103,14 +102,6 @@ export default function App() {
           element={
             <ProtectedRoute minAuthLevel={2}>
               <ProjectPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/projects2/:projectId"
-          element={
-            <ProtectedRoute minAuthLevel={2}>
-              <ProjectPage_v2 />
             </ProtectedRoute>
           }
         />
